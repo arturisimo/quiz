e1e439b651b4ac683f9c8b7adb58800e3d40be16
arturisimo <arturisimo@gmail.com>
2015-05-31 11:46:55 +0200

    Primera página y favicon

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//importar paquetes con los middlewares
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,9 +6,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//importar routes
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
+//generar aplicacion
 var app = express();
 
 // view engine setup
@@ -15,15 +17,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//asociar rutas a los gestores
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,6 +57,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
