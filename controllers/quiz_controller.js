035c4f0ec18f7e33b8efb2f5678cf22f26c07793
arturisimo <arturisimo@gmail.com>
2015-06-02 00:10:30 +0200

    layout footer

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,10 @@
+exports.index = function(request, response){
+	 response.render('', {title: 'Bienvenid@ a Quiz', file: 'index'});
+}
+
+
 exports.question = function(request, response){
-	 response.render('quizes/question', {title: 'Bienvenid@ a Quiz', question: 'Capital de Italia'});
+	 response.render('quizes/question', {title: 'Bienvenid@ a Quiz', question: 'Capital de Italia', file: 'quizes/question'});
 }
 
 exports.answer = function(request, response){
@@ -7,9 +12,13 @@ exports.answer = function(request, response){
 	var answer = request.query.answer;
 	var feedback = answer && answer.toLowerCase() === "roma" ? "Correcto" : "Incorrecto";	
 
-	response.render('quizes/answer', { title: 'Bienvenid@ a Quiz', feedback: feedback });
+	response.render('quizes/answer', { title: 'Bienvenid@ a Quiz', feedback: feedback, file : 'quizes/answer' });
 }
 
 exports.author = function(request, response){
-	response.render('author', { title: 'Bienvenid@ a Quiz' });
+	response.render('author', { title: 'Bienvenid@ a Quiz', file: 'author' });
+}
+
+exports.error = function(request, response) {
+	response.render('error', { title: 'ERROR 404', file : 'error'});
 }
--- routes/index.js
+++ routes/index.js
@@ -4,36 +4,10 @@ var router = express.Router();
 //importar routes
 var controller = require('../controllers/quiz_controller');
 
-
-/* GET home page. */
-router.get('/', function(request, response, next) {
-  response.render('index', { title: 'Bienvenid@ a Quiz' });
-});
-
-
+router.get('/', controller.index);
 router.get('/quizes/question', controller.question);
 router.get('/quizes/answer', controller.answer);
 router.get('/author', controller.author);
-/*
-router.get('/quizes/question', function(request, response) {
-     response.render('quizes/question', {title: 'Bienvenid@ a Quiz'});
-});
-
-router.get('/quizes/answer', function(request, response) {
-
-	var feedback = request.query.answer.toLowerCase() === "roma" ? "Correcto" : "Incorrecto";
-	response.render('quizes/answer', { title: 'Bienvenid@ a Quiz', feedback: feedback });
-	
-});
-
-router.get('/quizes/author', function(request, response) {
-     response.render('quizes/author');
-});
-*/
-
-router.get('*', function(request, response) {
-	response.render('error', { message: 'ERROR 404'})
-}) ;
-
+router.get('*', controller.error);
 
 module.exports = router;
