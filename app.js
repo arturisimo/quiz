df8b782daba8cbb5966e53b4db58515552b68a8b
arturisimo <arturisimo@gmail.com>
2015-08-03 00:07:41 +0200

    Modulo 8

--- app.js
+++ app.js
@@ -30,11 +30,13 @@ app.use(partials());
 app.use('/', routes);
 
 // catch 404 and forward to error handler
+/*
 app.use(function(req, res, next) {
   var err = new Error('Not Found!!');
   err.status = 404;
+  err.message="Error 404";
   next(err);
-});
+});*/
 
 // error handlers
 
@@ -48,7 +50,8 @@ if (app.get('env') === 'development') {
       description: "Ooops ha habido un fallo técnico!",
       file: 'error',
       message: err.message,
-      error: err
+      error: err,
+      classMenu: { index:false, quiz: false, author:false }
     });
   });
 }
--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,6 +1,5 @@
 var models = require('../models/models.js');
 
-
 exports.load = function(request, response, next, id){
 	models.Quiz.findById(id).then(function(quiz){
 		if(quiz){
@@ -13,28 +12,43 @@ exports.load = function(request, response, next, id){
 }
 
 exports.index = function(request, response){
-	 response.render('', {title: 'Bienvenid@ a Quiz', description: 'El portal donde puedes crear tus propios juegos', file: 'index'});
+	 var data = {title: 'Bienvenid@ a Quiz', description: 'El portal donde puedes crear tus propios juegos', file: 'index', classMenu: { index:true, quiz: false, author:false }};
+	 response.render('', data);
 }
 
 exports.quizes = function(request, response, next){
-	 models.Quiz.findAll().then(function(quizes){
-		response.render('quizes/question', {title: 'Preguntas', description: 'Prueba tus conocimientos', quizes: quizes, file: 'quizes/question'});
+	
+	var data = { title: 'Preguntas', description: 'Prueba tus conocimientos', file: 'quizes/question', classMenu: { index:false, quiz: true, author:false }};
+	
+	models.Quiz.findAll().then(function(quizes){
+		data.quizes = quizes; 
+		response.render('quizes/question', data);
 	}).catch(function(error){next(error)});
 }
 
 exports.quiz = function(request, response){
 	var quizes = new Array(request.quiz);
-	response.render('quizes/question', {title: 'Pregunta #' +  request.quiz.id, description: 'Prueba tus conocimientos', quizes: [request.quiz], file: 'quizes/question'});
+	var data = { title: 'Pregunta #' +  request.quiz.id, 
+				 description: 'Prueba tus conocimientos', 
+				 quizes: [request.quiz], 
+				 file: 'quizes/question',
+				 classMenu: { index:false, quiz: true, author:false }
+				}
+	response.render('quizes/question', data);
 }
 
 exports.create = function(request, response){
 	var quiz = {id: "", preguntas:"", respuestas:"", tematica: ""};
-	response.render('quizes/add', {title: 'Nueva Pregunta', description: 'Añade una nueva pregunta', quiz: quiz, file: 'quizes/add'});
+	response.render('quizes/add', {title: 'Nueva Pregunta', description: 'Añade una nueva pregunta', quiz: quiz, file: 'quizes/add', classMenu: { index:false, quiz: true, author:false }});
 }
 
 exports.update = function(request, response){
 	var quiz = request.quiz;
-	response.render('quizes/add', {title: 'Modificación de la pregunta ' +  quiz.id, description: 'Añade una nueva pregunta', quiz: quiz, file: 'quizes/add'});
+	response.render('quizes/add', {title: 'Modificación de la pregunta ' +  quiz.id, description: 'Añade una nueva pregunta', quiz: quiz, file: 'quizes/add', classMenu: { index:false, quiz: true, author:false }});
+}
+
+exports.edit = function(request, response){
+		
 }
 
 exports.insert = function(request, response){
@@ -42,44 +56,41 @@ exports.insert = function(request, response){
 	var preguntas = request.body.preguntas;
 	var respuestas = request.body.respuestas;
 	var tematica = request.body.tematica;
-	//var post =  {id: id , preguntas: preguntas, respuestas:respuestas, tematica: tematica};
-	//console.log(quiz);
-	if(id != ''){
-		var post =  {id: id , preguntas: preguntas, respuestas:respuestas, tematica: tematica};
-		var quiz = models.Quiz.build(post);
-		quiz.updateAttributes({preguntas: preguntas, respuestas:respuestas, tematica: tematica}).then(function(){
-			response.redirect('/');
-		}).catch(function(error){next(error)});
+
+	if(id!=''){
+		models.Quiz.findById(id).then(function(quiz){
+			if(quiz){
+				quiz.preguntas = preguntas;
+				quiz.respuestas = respuestas;
+				quiz.tematica = tematica;
+
+				quiz.save({fields: ["preguntas", "respuestas", "tematica"]}).then(function(){
+					response.redirect('/quizes');
+				})
+			}
+		});
 	} else {
+
 		var quiz = models.Quiz.build({preguntas: preguntas, respuestas:respuestas, tematica: tematica});
 		quiz.save().then(function(){
-			response.redirect('/');
+			response.redirect('/quizes');
 		}).catch(function(error){next(error)});
 	}
 	
 }
 
+exports.delete = function(request, response){
+		request.quiz.destroy().then(function(){
+			response.redirect('/quizes');
+		}).catch(function(error){next(error)});
+}
+
 exports.search = function(request, response,  next){
 	var search = request.body.search;
-	var tematica = request.body.tematica;
-	var where = "";
-	var criterio = "";
-	if (tematica!=''){
-		where = where + "tematica = '" + tematica + "'";
-		criterio = criterio + " Temática: " + tematica;
-	}
-	if (search!=''){
-		if(where!=''){
-			where = where + " and "
-		}
-		where = where + "lower(preguntas) like '%" + search.toLowerCase() + "%";
-		criterio = criterio + " Busqueda: '" + search + "'";
-	}
-
-	//var where = "lower(preguntas) like '%" + search.toLowerCase() + "%";
-	
-	models.Quiz.findAll({where: [where]}).then(function(quizes){
-		response.render('quizes/question', {title: 'Preguntas ' + criterio, description: quizes.length > 0  ? 'Prueba tus conocimientos' : 'no hay preguntas con este criterio', quizes: quizes, file: 'quizes/question'});
+	var where = "lower(preguntas) like '%" + search.toLowerCase() + "%'";
+	var criterio = " Busqueda: '" + search + "'";
+	models.Quiz.findAll({order: [['preguntas', 'DESC']], where: [where]}).then(function(quizes){
+		response.render('quizes/question', {title: 'Preguntas ' + criterio, description: quizes.length > 0  ? 'Prueba tus conocimientos' : 'no hay preguntas con este criterio', quizes: quizes, file: 'quizes/question', classMenu: { index:false, quiz: true, author:false }});
 	}).catch(function(error){next(error)});
 
 }
@@ -94,13 +105,15 @@ exports.answer = function(request, response){
 									   description: '', 
 									   respuesta: answer,
 									   feedback: feedback, 
-									   file : 'quizes/answer' });
+									   file : 'quizes/answer',
+									   classMenu: { index:false, quiz: true, author:false }});
 }
 
 exports.author = function(request, response){
-	response.render('author', { title: 'Bienvenid@ a Quiz', description: 'by arturo', file: 'author' });
+	response.render('author', { title: 'Bienvenid@ a Quiz', description: 'by arturo', file: 'author', classMenu: { index:false, quiz: false, author:true } });
 }
 
+
 exports.error = function(request, response) {
-	response.render('error', { title: 'ERROR 404', description: 'error', file : 'error'});
+	response.render('error', { title: 'ERROR 404', description: '', message:'Página no encontrada', file : 'error', classMenu: { index:false, quiz: false, author:false }});
 }
